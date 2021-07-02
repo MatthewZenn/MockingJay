54d71bf6f5fbca14434ef997c0b8f15b6a0cdbff
Matthew Zenn <spyguy57@gmail.com>
2021-07-02 09:20:20 -0400

    Bug fixes

--- logic.js
+++ logic.js
@@ -3,8 +3,6 @@ const fakeButton = document.getElementById("picture");
 const image = document.getElementById("photo");
 const name1 = document.getElementById("name-input");
 const user1 = document.getElementById("user-input");
-var text = document.getElementById("tweet").value;
-var textString = text.toString()
 
 fakeButton.addEventListener("click", function() {
     realButton.click()
@@ -24,14 +22,8 @@ realButton.addEventListener("change", function() {
 });
 
 function update() {
-    var text = document.getElementById("tweet").value;
-    var textString = text.toString()
     document.getElementById("name").innerHTML = name1.value;
     document.getElementById("username").innerHTML = ' @'+user1.value;
-    var syntax = textString.split(" ");
-    for (let i = 0; i < syntax.length; i++) {
-        console.log(syntax[i].startsWith("#"));
-    }
 }
 
 function printresult() {
